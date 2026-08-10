3cc9406a071bbc9892d8cab66c6d891e0fab0746
Big Jim <bigjim@BignoMacBook-Pro.local>
2026-08-10 15:13:55 +0900

    fix(platform): use e2-small nodes

--- infrastructure/modules/platform/gke-nodepool/main.tf
+++ infrastructure/modules/platform/gke-nodepool/main.tf
@@ -39,7 +39,7 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "e2-small"
     tags         = ["gke-node", "${var.stack}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"

ca48c3a0c8c44803d28fa6cc55c54926fe2d5a1d
Big Jim <bigjim@BignoMacBook-Pro.local>
2026-08-12 16:31:33 +0900

    feat(argocd): add argocd bootstrap script

---
+++ argocd/scripts/bootstrap.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+TEMPLATE_DIR="${SCRIPT_DIR}/../templates"
+
+argo_cd_namespace="${ARGO_CD_NAMESPACE:-argocd}"
+
+ensure_namespace() {
+    if ! kubectl get namespace "${argo_cd_namespace}" > /dev/null 2>&1; then
+        kubectl create namespace "${argo_cd_namespace}"
+    fi
+}
+
+ensure_argocd() {
+    # From the docs: https://argo-cd.readthedocs.io/en/stable/getting_started/#1-install-argo-cd
+    kubectl apply -n "${argo_cd_namespace}" -f "${TEMPLATE_DIR}/argocd.yml" \
+        --server-side \
+        --force-conflicts
+}
+
+ensure_namespace
+ensure_argocd
+echo "---"
+echo
+echo "ArgoCD bootstrapped."
+echo
+echo "Access ArgoCD by running the following command:"
+echo "kubectl port-forward svc/argocd-server -n ${argo_cd_namespace} 8080:443"
+echo "and accessing: https://localhost:8080/"
+echo
